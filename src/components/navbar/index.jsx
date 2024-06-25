71fd17ea89f2e8d064d99921edceeb3fdff7d60c
Gustavo Krause <gugukrause@gmail.com>
2024-06-25 19:11:21 -0300

    criando navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <h1>Mercado Krause</h1>
+        <nav>
+            <ul>
+                <li>Sobre</li>
+                <li>Produtos</li>
+            </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Navbar;
